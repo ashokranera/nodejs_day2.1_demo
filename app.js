f69f2406e87403181d2c65ab1ab4dd586f621a96
ashokranera <ashokranera@gmail.com>
2021-08-10 16:41:20 +0530

    sum and mul of two number

---
+++ app.js
@@ -0,0 +1,5 @@
+var task = require('./task')
+
+console.log(task.sum(12,13));
+
+console.log(task.mul(2,3));
---
+++ task.js
@@ -0,0 +1,16 @@
+var sum = function(a,b){
+
+    return `the sum of the two number is ${a+b}`;
+   
+   };
+
+   var mul = function(a,b){
+
+    return `the mul  of the two number is ${a*b}`;
+   
+   };
+   module.exports = {
+       sum : sum,
+       mul : mul
+
+   };
